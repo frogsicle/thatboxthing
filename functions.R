6f7df71c1ea983acdf7dde963aeecf01dc868f47
frogsicle <a7denton@gmail.com>
2017-05-18 00:25:28 +0200

    take 1, cannonical shapes

--- functions.R
+++ functions.R
@@ -1,16 +1,29 @@
+# dummy shape
 ashape <- matrix(c(1,1,0,
                    1,0,0,
                    1,0,0), 3, 3)
 
-rotate.cc <- function(ashape){
-  out <- apply(ashape, 1, rev)
-  return(out)
-}
 
+### everything needed to get scorable, cannonical shapes
+# total scoring
 score.shape <- function(ashape){
-  return(sum(ashape)) #fix
+  s.vert <- score.vert(ashape)
+  s.horiz <- score.horiz(ashape)
+  return(c(s.vert, s.horiz)) 
 }
 
+# directional scoring
+score.vert <- function(ashape){
+  score <- sum(rowSums(ashape) * nrow(ashape):1)
+  return(score)
+}
+
+score.horiz <- function(ashape){
+  return(score.vert(t(ashape)))
+}
+
+## home
+# to top and fill
 home.shape.vert <- function(ashape, padrow=13){
   i <- 1
   so_far_only_zilch <- TRUE
@@ -35,6 +48,7 @@ home.shape.vert <- function(ashape, padrow=13){
   return(ashape)
 }
 
+# to left and fill
 home.shape.horiz <- function(ashape, padrow=13){
   tshape <- t(ashape)
   tshape <- home.shape.vert(tshape)
@@ -42,6 +56,22 @@ home.shape.horiz <- function(ashape, padrow=13){
   return(ashape)
 }
 
+# to top left
+home.shape <- function(x, pad=13) {
+  x <- home.shape.vert(x, pad)
+  x <- home.shape.horiz(x, pad)
+  return(x)
+}
+
+## rotate
+# rotate once
+rotate.cc <- function(ashape){
+  out <- apply(ashape, 1, rev)
+  return(out)
+}
+
+
+# all 8 rotations
 rotate.all <- function(ashape){
   out <- list()
   i <- 1
@@ -55,4 +85,25 @@ rotate.all <- function(ashape){
     ashape <- rotate.cc(ashape)
   }
   return(out)
+}
+## combining 
+# rotate and home
+get.comparables <- function(ashape){
+  rotations <- rotate.all(ashape)
+  homies <- lapply(rotations, home.shape)
+  return(homies)
+}
+
+score.all <- function(homies){
+  out <- sapply(homies, score.shape)
+  rownames(out) <- c('vert','horiz')
+  return(out)
+}
+
+get.cannonical <- function(ashape){
+  homies <- get.comparables(ashape)
+  scores <- score.all(homies)
+  # vertical before horizontal scoring
+  rankedscores <- order(scores[1,], scores[2,], decreasing=T)
+  return(homies[[rankedscores[1]]])
 }
